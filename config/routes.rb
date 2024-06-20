d9e76f4c784b2d539827d72dff3bf8e4e8551b55
Jerry Mathew <jer2ymatheww@gmail.com>
2024-06-20 11:00:40 +0530

    Devise controllers and views refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :students
-  devise_for :teachers
+  devise_for :students, controllers: { registrations: 'students/registrations' }
+  devise_for :teachers, controllers: { registrations: 'teachers/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
